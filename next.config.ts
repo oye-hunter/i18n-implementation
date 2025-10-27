338edc5f5fe81d3c2950ba46277b191ac4adcf79
oye-hunter <muhammadhassanmughal47@gmail.com>
2025-10-27 16:56:04 +0500

    update: next config to ignore eslit errors

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig: NextConfig & { eslint?: { ignoreDuringBuilds?: boolean } } = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
